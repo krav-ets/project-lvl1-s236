03b20f6917e59e68452c761d11694c7611a2d7c1
krav-ets <krav-ets@yandex.ru>
2018-03-09 18:47:51 +0300

    checkpoint 9: fix isPrime function

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -4,11 +4,11 @@ import getRandomNum from '../lib/random';
 const description = 'Is this number prime?';
 
 const isPrime = (num) => {
-  if (num === 1) return 'no';
+  if (num === 1) return false;
   const i = Math.floor(num / 2);
   const iter = (n) => {
-    if (n === 1) return 'yes';
-    if (num % n === 0) return 'no';
+    if (n === 1) return true;
+    if (num % n === 0) return false;
     return iter(n - 1);
   };
   return iter(i);
@@ -16,7 +16,7 @@ const isPrime = (num) => {
 
 const genQuestionAndAnswer = () => {
   const quest = getRandomNum(1, 100);
-  const rightAnswer = isPrime(quest);
+  const rightAnswer = isPrime(quest) ? 'yes' : 'no';
   return [quest, rightAnswer];
 };
 
